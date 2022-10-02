eea5a8caae56b9e1525a5f3442e9a7c54a57dc55
Roman-Moskalenko <92758017+Roman-Moskalenko@users.noreply.github.com>
2022-10-02 23:41:02 +0300

    hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import SimpleLightboxStyle from "simplelightbox/dist/simple-lightbox.min.css"
 // Change code below this line
-console.log(SimpleLightbox)
+console.log(SimpleLightboxStyle)
 console.log(galleryItems);
