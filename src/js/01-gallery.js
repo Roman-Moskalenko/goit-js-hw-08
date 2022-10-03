0f77b0ab1c0c0292a423e5cb201a663e56fb752b
Roman-Moskalenko <92758017+Roman-Moskalenko@users.noreply.github.com>
2022-10-03 23:19:39 +0300

    hw 8.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,33 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-import SimpleLightboxStyle from "simplelightbox/dist/simple-lightbox.min.css"
-// Change code below this line
-console.log(SimpleLightboxStyle)
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+// // Change code below this line
+
+const galleryContainer = document.querySelector( '.gallery')
+
+const galleryMarkup = createImageCardMarkup(galleryItems);
+
+function createImageCardMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description }) => {
+    return`<div class="gallery__item">
+    <a class="gallery__link" 
+    href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+   }).join(" ")  
+}
+galleryContainer.insertAdjacentHTML( 'beforeend', galleryMarkup)
+const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+      captionType: 'alt',
+    });
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALKEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate(e) {
+  localStorage.setItem(LOCALKEY, e.seconds);
+}
+
+const savedTime = localStorage.getItem(LOCALKEY);
+if (savedTime) {
+  const currentTime = JSON.parse(savedTime);
+  player.setCurrentTime(currentTime);
+}
